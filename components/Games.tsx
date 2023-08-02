a57f06de26acf0470856c0d7ca3f928791d4d473
benshaw3283 <benshaw3283@gmail.com>
2023-08-02 22:33:56 +1000

    map set up

--- components/Games.tsx
+++ components/Games.tsx
@@ -2,8 +2,23 @@ import { useQuery } from "@tanstack/react-query";
 
 import React from 'react'
 
-const fetchGames = () => {
-        
+const key = process.env.NBA_API_KEY
+let todaysDate = new Date().toISOString().slice(0, 10)
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com',
+        'X-RapidAPI-Key': "efd4542df6msh04ed18037300093p17f43ejsnd4fbc1b9f248"
+    }
+}
+
+//${todaysDate}
+
+const fetchGames = async() => {
+    const response = await fetch(`https://api-nba-v1.p.rapidapi.com/games?date=2023-03-05`, options);
+    const result = await response.json()
+    
+    return result;
 }
 
 const Games = () => {
@@ -11,12 +26,31 @@ const Games = () => {
 
     const { isLoading, error, data } = useQuery({
         queryKey: ['gamesData'],
-        queryFn: ()=>  fetchGames()
+        queryFn: ()=> fetchGames()
       })
+
 
   return (
     <div>
+       {data ? (
         
+       data.response.map((game: any, index: number)=> (
+        <div key={index}> 
+            <p>{game.teams.visitors.name}</p>
+            <p>{game.teams.home.name}</p>
+        </div>
+        ))
+        
+       ) : (
+        <span>No games on today!</span>
+       )}
+
+       <div>
+        {isLoading ? (
+            <span>Loading...</span>
+        ): 
+        <div> </div>}
+       </div>
     </div>
   )
 }
